cad2b2f575c3dc707a574e0f433507c77642d1de
span-qwer <153086851+span-qwer@users.noreply.github.com>
2024-01-06 11:34:53 +0400

    Update useLoading.ts

--- composables/useLoading.ts
+++ composables/useLoading.ts
@@ -1,30 +1,48 @@
-import { type Ref, ref } from 'vue'
+import { ref } from 'vue'
 
-export default function (fun: Function): any {
-  const LoadingCounter: Ref<number> = ref(0)
-  const Loading: Ref<boolean> = ref(false)
+export default function (fun) {
+  const LoadingCounter = ref(0)
+  const Loading = ref(false)
   const show = ref(false)
 
-  return new Proxy(fun, {
-    get: function (tar, prop, reactive) {
-      if (prop === 'show') return show.value
-      if (prop === 'Loading') return Loading.value
-      if (prop === 'LoadingCounter') return LoadingCounter.value
-      return Reflect.get(tar, prop, reactive)
-    },
-    apply: function (tar, _this, args) {
-      let r = tar.apply(_this, args) as Promise<any>
-      if (r && typeof r.finally === 'function') {
-        LoadingCounter.value += 1
-        Loading.value = !!LoadingCounter.value
+  // return new Proxy(fun, {
+  //   get: function (tar, prop, reactive) {
+  //     if (prop === 'show') return show.value
+  //     if (prop === 'Loading') return Loading.value
+  //     if (prop === 'LoadingCounter') return LoadingCounter.value
+  //     return Reflect.get(tar, prop, reactive)
+  //   },
+  //   apply: function (tar, _this, args) {
+  //     let r = tar.apply(_this, args)
+  //     if (r && typeof r.finally === 'function') {
+  //       LoadingCounter.value += 1
+  //       Loading.value = !!LoadingCounter.value
 
-        r = r.finally(() => {
-          show.value = true
-          LoadingCounter.value -= 1
-          Loading.value = !!LoadingCounter.value
-        }) as Promise<any>
-      }
-      return r
+  //       r = r.finally(() => {
+  //         show.value = true
+  //         LoadingCounter.value -= 1
+  //         Loading.value = !!LoadingCounter.value
+  //       })
+  //     }
+  //     return r
+  //   }
+  // })
+  function newfn(...args) {
+    let r = fun.apply(this, args)
+    if (r && typeof r.finally === 'function') {
+      LoadingCounter.value += 1
+      Loading.value = !!LoadingCounter.value
+
+      r = r.finally(() => {
+        show.value = true
+        LoadingCounter.value -= 1
+        Loading.value = !!LoadingCounter.value
+      })
     }
-  })
+    return r
+  }
+  newfn.LoadingCounter = LoadingCounter
+  newfn.Loading = Loading
+  newfn.show = show
+  return newfn
 }
